01885bba8e5d3477af698fd3e4818ea7155630cf
Raj Dhadda <raj.dhadda@trescal.com>
2024-08-01 13:46:36 +0000

    added add to bag functionality

--- products/views.py
+++ products/views.py
@@ -66,4 +66,4 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'products/products_details.html', context)
